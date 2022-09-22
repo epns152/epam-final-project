44fe6dfc1de45d1ce05fddbdfbf596a61203e50d
Roman <pavlenko.roman@lll.kpi.ua>
2022-09-22 18:35:48 +0300

    Changed sql script

--- sql/epam final project script.sql
+++ sql/epam final project script.sql
@@ -1,121 +1,84 @@
-CREATE DATABASE  IF NOT EXISTS `demo` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `demo`;
--- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
---
--- Host: localhost    Database: demo
--- ------------------------------------------------------
--- Server version	8.0.30
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `accounts`
---
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
-DROP TABLE IF EXISTS `accounts`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `accounts` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `account_name` varchar(45) NOT NULL,
-  `balance_amount` decimal(10,2) unsigned NOT NULL,
-  `Users_id` int NOT NULL,
-  `unblock_request` int NOT NULL DEFAULT '0',
-  `account_status` varchar(15) NOT NULL DEFAULT 'unblocked',
-  PRIMARY KEY (`id`),
-  KEY `fk_Accounts_Users_idx` (`Users_id`),
-  CONSTRAINT `fk_Accounts_Users` FOREIGN KEY (`Users_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `accounts`
---
 
-LOCK TABLES `accounts` WRITE;
-/*!40000 ALTER TABLE `accounts` DISABLE KEYS */;
-/*!40000 ALTER TABLE `accounts` ENABLE KEYS */;
-UNLOCK TABLES;
+CREATE SCHEMA IF NOT EXISTS `demo` DEFAULT CHARACTER SET utf8mb3 ;
+USE `demo` ;
 
---
--- Table structure for table `payments`
---
+-- -----------------------------------------------------
+-- Table `demo`.`users`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `demo`.`users` ;
 
-DROP TABLE IF EXISTS `payments`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `payments` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `price` decimal(9,2) unsigned NOT NULL,
-  `payment_name` varchar(45) NOT NULL,
-  `creation_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `payment_status` int NOT NULL DEFAULT '1',
-  `users_id` int NOT NULL,
-  `account_id` int NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  KEY `fk_payments_users1_idx` (`users_id`),
-  CONSTRAINT `fk_payments_users1` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE IF NOT EXISTS `demo`.`users` (
+                                              `id` INT NOT NULL AUTO_INCREMENT,
+                                              `login` VARCHAR(45) NOT NULL,
+    `password` VARCHAR(45) NOT NULL,
+    `firstname` VARCHAR(45) NOT NULL,
+    `lastname` VARCHAR(45) NOT NULL,
+    `user_status` VARCHAR(10) NOT NULL DEFAULT 'unblocked',
+    `user_role` VARCHAR(10) NOT NULL DEFAULT 'customer',
+    `registration_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `login_UNIQUE` (`login` ASC) VISIBLE,
+    FULLTEXT INDEX `idx_user_login` (`login`) VISIBLE)
+    ENGINE = InnoDB
+    AUTO_INCREMENT = 5
+    DEFAULT CHARACTER SET = utf8mb3;
 
---
--- Dumping data for table `payments`
---
 
-LOCK TABLES `payments` WRITE;
-/*!40000 ALTER TABLE `payments` DISABLE KEYS */;
+-- -----------------------------------------------------
+-- Table `demo`.`accounts`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `demo`.`accounts` ;
 
-/*!40000 ALTER TABLE `payments` ENABLE KEYS */;
-UNLOCK TABLES;
+CREATE TABLE IF NOT EXISTS `demo`.`accounts` (
+                                                 `id` INT NOT NULL,
+                                                 `account_name` VARCHAR(45) NOT NULL,
+    `balance_amount` DECIMAL(10,2) UNSIGNED NOT NULL,
+    `Users_id` INT NOT NULL,
+    `unblock_request` INT NOT NULL DEFAULT '0',
+    `account_status` VARCHAR(15) NOT NULL DEFAULT 'unblocked',
+    `card_id` BIGINT GENERATED ALWAYS AS ((53531100 + `id`)) VIRTUAL,
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `card_id_UNIQUE` (`card_id` ASC) VISIBLE,
+    INDEX `fk_Accounts_Users_idx` (`Users_id` ASC) VISIBLE,
+    CONSTRAINT `fk_Accounts_Users`
+    FOREIGN KEY (`Users_id`)
+    REFERENCES `demo`.`users` (`id`))
+    ENGINE = InnoDB
+    DEFAULT CHARACTER SET = utf8mb3;
 
---
--- Table structure for table `users`
---
 
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `login` varchar(45) NOT NULL,
-  `password` varchar(45) NOT NULL,
-  `firstname` varchar(45) NOT NULL,
-  `lastname` varchar(45) NOT NULL,
-  `user_status` varchar(10) NOT NULL DEFAULT 'unblocked',
-  `user_role` varchar(10) NOT NULL DEFAULT 'customer',
-  `registration_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `login_UNIQUE` (`login`),
-  FULLTEXT KEY `idx_user_login` (`login`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- -----------------------------------------------------
+-- Table `demo`.`payments`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `demo`.`payments` ;
 
---
--- Dumping data for table `users`
---
+CREATE TABLE IF NOT EXISTS `demo`.`payments` (
+                                                 `id` INT NOT NULL AUTO_INCREMENT,
+                                                 `price` DECIMAL(9,2) UNSIGNED NOT NULL,
+    `payment_name` VARCHAR(45) NOT NULL,
+    `creation_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `payment_status` INT NOT NULL DEFAULT '1',
+    `users_id` INT NOT NULL,
+    `account_number_received` BIGINT UNSIGNED NOT NULL,
+    `account_number_sent` BIGINT UNSIGNED NOT NULL DEFAULT '0',
+    PRIMARY KEY (`id`),
+    INDEX `fk_payments_users1_idx` (`users_id` ASC) VISIBLE,
+    CONSTRAINT `fk_payments_users1`
+    FOREIGN KEY (`users_id`)
+    REFERENCES `demo`.`users` (`id`))
+    ENGINE = InnoDB
+    AUTO_INCREMENT = 5
+    DEFAULT CHARACTER SET = utf8mb3;
 
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` VALUES (3,'admin','21232f297a57a5a743894a0e4a801fc3','admin','admin','unblocked','admin','2022-08-22 00:19:10');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2022-09-01 16:48:01
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
